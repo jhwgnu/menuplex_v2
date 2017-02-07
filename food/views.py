d21e4da9637273d2e8a8e6a3a3cc670b1dfc55c6
sangjaekang <sangjaekang48@gmail.com>
2017-02-07 19:40:08 +0900

    식당리스트 구현

--- food/views.py
+++ food/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 
 def detail(request,school_id):
-    restaurant_list = Restaurant.objects.filter(pk=school_id)
+    restaurant_list = Restaurant.objects.filter(school_id=school_id)
     context = {'school_id':school_id,'restaurant_list': restaurant_list}
 
     return render(request,'food/detail.html',context)
